e07a104a4bccc98e8326e411229ad949192d7a23
exima09 <dawidjuszczak01@gmail.com>
2018-12-19 02:11:26 +0100

    add auth users

--- Backend/rebuild-db.sh
+++ Backend/rebuild-db.sh
@@ -4,10 +4,9 @@ bin/console doctrine:database:drop --force \
     && rm src/Migrations/* \
     && yes |bin/console doctrine:migrations:diff \
     && bin/console doctrine:migrations:migrate \
-    && yes | bin/console doctrine:fixtures:load \
     && rm -r config/jwt \
     && mkdir  config/jwt \
-    && openssl genrsa -out config/jwt/private.pem -aes256 4096 \
+    && openssl genrsa -out config/jwt/private.pem 4096 \
     && openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem \
     && openssl rsa -in config/jwt/private.pem -out config/jwt/private2.pem \
     && mv config/jwt/private.pem config/jwt/private.pem-back \
--- Backend/src/Entity/User.php
+++ Backend/src/Entity/User.php
@@ -3,11 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -30,6 +31,11 @@ class User
      * @ORM\ManyToOne(targetEntity="App\Entity\Roles", inversedBy="users")
      */
     private $Role;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $username;
 
     public function getId(): ?int
     {
@@ -71,4 +77,82 @@ class User
 
         return $this;
     }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return array('ROLE_USER');
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+    /**
+     * @return mixed
+     */
+    public function serialize()
+    {
+        return $this->serialize([
+            $this->id,
+            $this->username,
+            $this->password
+        ]);
+    }
+
+    /**
+     * @param $string
+     */
+    public function unserialize($string)
+    {
+        list(
+            $this->id,
+            $this->username,
+            $this->password
+            ) = $this->unserialize($string, ['allowed_classes' => false]);
+    }
+
 }
