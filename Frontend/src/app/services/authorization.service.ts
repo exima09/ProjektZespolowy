8c0ddbb766d58333caa30469439b984b35c825e1
sandrapol <44498259+sandrapol@users.noreply.github.com>
2019-01-15 19:17:20 +0100

    Authorization service (#15)
    * components moved to correct files
    
    * empty lines deleted
    
    * authorizationService with login and register method, and snack-bars
    
    * login method updated
    
    * <weszlo> deleted

---
+++ Frontend/src/app/services/authorization.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { MatSnackBar, MatSnackBarConfig } from "@angular/material/snack-bar"
+import { map, catchError } from 'rxjs/operators';
+
+import { HttpErrorResponse, HttpHeaders, HttpClient } from "@angular/common/http";
+import { throwError, Subject } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationService {
+
+    constructor(private http: HttpClient, public snackBar: MatSnackBar) { }
+
+    register(login: string, password: string) {
+        let headers = new HttpHeaders({ "username": login, "password": password });
+        return this.http.post("/api/register", headers).pipe(catchError(err => this.errorHandler(err)));
+    }
+
+    login(login: string, password: string) {
+        let headers = new HttpHeaders({ "username": login, "password": password });
+        return this.http.post("/api/login_check", headers).pipe(catchError(err => this.errorHandler(err)));
+    }
+
+    errorHandler(error: HttpErrorResponse) {
+        if (error.name) {
+            this.snackBar.open(error.name, error.statusText, {
+                duration: 2000,
+                panelClass: ['service-snackbar']
+            });
+
+            return throwError(error.name + '\n details: ' + error.statusText);
+        } else {
+            this.snackBar.open('error', 'The request can not be executed', {
+                duration: 5000,
+                panelClass: ['service-snackbar']
+            });
+            return throwError(error);
+        }
+    }
+}
