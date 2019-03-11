5d396659403b884ab3d1b1e2e5f4bca4a92981d6
Kamil <pridecry@gmail.com>
2019-03-12 00:37:41 +0100

    Added prisoner details html&css

--- Frontend/src/app/components/content/prisoners/prisoner-details/prisoner-details.component.ts
+++ Frontend/src/app/components/content/prisoners/prisoner-details/prisoner-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PrisonerService } from 'src/app/services/prisoner/prisoner.service';
 import { Prisoner } from 'src/app/models/prisoner/prisoner.model';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-prisoner-details',
@@ -11,17 +11,23 @@ import {MatGridListModule} from '@angular/material/grid-list';
 export class PrisonerDetailsComponent implements OnInit {
   headerOfSite = 'Szczegóły więźnia';
   prisoner: Prisoner;
-  constructor(private service: PrisonerService) { }
+  id: number;
+  constructor(private service: PrisonerService, private route: Router) { }
 
   ngOnInit() {
-    this.prisoner = example;
+    let urlArr = this.route.url.split("/");
+    this.id = Number(urlArr[2]);
+
+    this.service.getPrisonerById(this.id).subscribe((res: any) => {
+      this.prisoner = JSON.parse(res.prisoner);
+    });
   }
 }
 
 const example: Prisoner = {
-  firstName: "Jan",
-  lastName: "Gierasimiuk",
-  joinDate: "06.09.2015",
-  dateOfBirth: "03.07.1997",
-  cellId: 2
+  FirstName: "Jan",
+  LastName: "Gierasimiuk",
+  JoinDate: "06.09.2015",
+  DateOfBirth: "03.07.1997",
+  CellId: 2
 }
