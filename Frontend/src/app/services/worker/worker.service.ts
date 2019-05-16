69ecaa1c9b769bee01191cb5f220f934642bc280
Sandra <myszmilo2@gmail.com>
2019-05-16 21:15:40 +0200

    worker service added

---
+++ Frontend/src/app/services/worker/worker.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { MatSnackBar } from "@angular/material";
+import { getHeaders } from "../headers";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WorkerService {
+
+    constructor(private http: HttpClient, public snackBar: MatSnackBar) {
+    }
+
+    getUsers() {
+        return this.http.get('/api/user', getHeaders())
+            .pipe(catchError(err => this.errorHandler(err)));
+    }
+
+    getWorkers() {
+        return this.http.get('/api/worker', getHeaders())
+            .pipe(catchError(err => this.errorHandler(err)));
+    }
+
+    updateUser = (user, id: number) => this.http.patch(`/api/user/edit/${id}`, user, getHeaders())
+        .pipe(catchError(err => this.errorHandler(err)))
+
+    updateWorker = (worker, id: number) => this.http.patch(`/api/worker/edit/${id}`, worker, getHeaders())
+        .pipe(catchError(err => this.errorHandler(err)))
+
+    errorHandler(error: HttpErrorResponse) {
+        if (error.name) {
+            this.snackBar.open("Błąd", error.error.message, {
+                duration: 2000,
+                panelClass: ['service-snackbar']
+            });
+
+            return throwError(error.name + ": " + error.message);
+        } else {
+            this.snackBar.open("Błąd", "Żądanie nie może zostać przetworzone", {
+                duration: 5000,
+                panelClass: ['service-snackbar']
+            });
+
+            return throwError(error);
+        }
+    }
+}
